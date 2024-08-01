94b8521e534e93d2256257bf1c43f758fcec303f
jaloliddinmurodullaev <jaloliddinmurodullayev337@gmail.com>
2024-08-01 14:11:56 +0500

    models

--- app/models.py
+++ app/models.py
@@ -2,7 +2,44 @@ from django.db import models
 
 
 class Project(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=300)
     description = models.TextField()
-    technology = models.CharField(max_length=20)
+    technology = models.CharField(max_length=300)
     link = models.URLField(null=True, blank=True)
+    is_active = models.BooleanField(default=False)
+    seen_count = models.BigIntegerField(default=0, null=True)
+
+class Blog(models.Model):
+    title = models.CharField(max_length=300)
+    desctiption = models.TextField()
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=False)
+    seen_count = models.BigIntegerField(default=0, null=True)
+
+class Main(models.Model):
+    image = models.URLField()
+    github = models.URLField()
+    linkedin = models.URLField()
+    telegram = models.URLField()
+    instagram = models.URLField()
+    facebook = models.URLField()
+    twitter = models.URLField()
+    firstname = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    description = models.TextField()
+    about = models.TextField() 
+
+class Experience(models.Model):
+    company = models.CharField(max_length=100)
+    position = models.CharField(max_length=200)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    description = models.TextField()
+
+class Resume(models.Model):
+    firstname = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    description = models.TextField()
+    about = models.TextField()
+    
