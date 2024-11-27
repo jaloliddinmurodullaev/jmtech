57a05ca03c495ac9e263dedab8dc8a38aa3ebf60
mac <jaloliddinmurodullayev337@gmail.com>
2024-11-27 19:09:05 +0500

    Updated

--- app/forms.py
+++ app/forms.py
@@ -2,9 +2,32 @@ from django import forms
 from .models import Blog
 
 class ContactForm(forms.Form):
-    name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    message = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5}))
+    name = forms.CharField(
+        max_length = 100, 
+        widget = forms.TextInput(
+            attrs = {
+                'class': 'form-control',
+                'placeholder': 'Name'
+            }
+        )
+    )
+    email = forms.EmailField(
+        widget = forms.EmailInput(
+            attrs = {
+                'class': 'form-control',
+                'placeholder': 'Email'
+            }
+        )
+    )
+    message = forms.CharField(
+        widget = forms.Textarea(
+            attrs = {
+                'class': 'form-control', 
+                'rows': 5,
+                'placeholder': 'Type your message ...'
+            }
+        )
+    )
 
 class BlogForm(forms.Form):
     class Meta:
